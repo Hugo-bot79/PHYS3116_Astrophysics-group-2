22efeee42bae6ce07c681bb057252e589529d344
Hugo Chen <z5309553@ad.unsw.edu.au>
2025-09-27 22:16:39 +1000

    Thursday codes suggested by group members

---
+++ #csv file.py
@@ -0,0 +1,20 @@
+# Three lines of code that are discussed in the tutorial on Thursday, 25/09/2025
+# The code has been discussed and recorded by Hugo Chen, XingKun Feng and Abhinaya Jeyandran
+
+#first step, define and install all package that we need.
+import matplotlib.pyplot as plt
+import numpy as np
+import pandas as pd
+
+#Import csv file to python (testing whether it works)
+from csv import DictReader
+file_handle = open("Krause21.csv", "r", encoding = "utf-8")
+csv_reader = DictReader(file_handle, delimiter = ",")
+for row in csv_reader:
+    print(row)
+
+file_handle.close()
+
+#import csv file to pandas (real)
+Galaxydata_1 = pd.read_csv("Krause21.csv")
+print(Galaxydata_1)
