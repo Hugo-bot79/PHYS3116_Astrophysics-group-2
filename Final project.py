22730927d02308161371d205d2cf0cd759c84cc4
fxk0601-naomL <fxk0601@gmail.com>
2025-10-19 15:38:49 +1100

    import initial library

---
+++ Final project.py
@@ -0,0 +1,10 @@
+
+# import necessary libraries for data analysis
+
+#use numpy for numerical calculation-> arrays
+import numpy as np
+#use pandas for spreadsheet data
+import pandas as pd
+
+improt matplotlib.pyplot as plt
+from astropy.io import fits
